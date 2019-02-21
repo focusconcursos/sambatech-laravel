5bf474d12b5bd1d4adfdb5401e4d7d0e13bc5e9b
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2019-02-21 15:49:58 -0300

    Generate the upload url

--- src/Sambatech.php
+++ src/Sambatech.php
@@ -3,28 +3,79 @@
 namespace FocusConcursos\SambatechLaravel;
 
 
+use FocusConcursos\SambatechLaravel\Exception\CannotGenerateUploadUrlException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
 class Sambatech
 {
-    private $pid;
+    protected $sambaVideosBaseUrl = 'https://api.sambavideos.sambatech.com/v1';
 
-    private $token;
+    protected $pid;
 
+    protected $token;
+
+    protected $http;
+
+    /**
+     * Sambatech constructor.
+     *
+     * @param string $pid Project ID
+     * @param string $token Access token
+     */
     public function __construct(string $pid, string $token)
     {
         $this->pid = $pid;
         $this->token = $token;
+        $this->http = new Client();
     }
 
-    public function upload(string $path, array $metadata = []): string
-    {
-        return 'dummy';
-    }
-
+    /**
+     * Get the project id and token as an array
+     *
+     * @return array
+     */
     public function dumpConnection(): array
     {
         return [
             'pid' => $this->pid,
             'token' => $this->token
         ];
+    }
+
+    /**
+     * @param string $path Path to the target video file on the server
+     * @param array $metadata Parameters of the video, like name and description
+     * @return string The url of the video
+     * @throws CannotGenerateUploadUrlException
+     */
+    public function upload(string $path, array $metadata = []): string
+    {
+        $uploadUrl = $this->generateUploadUrl();
+
+        return 'dummy';
+    }
+
+    protected function generateUploadUrl(): string
+    {
+        $url = "{$this->sambaVideosBaseUrl}/medias";
+
+        try {
+            $res = $this->http->request('POST', $url, [
+                'query' => [
+                    'access_token' => $this->token,
+                    'pid' => $this->pid
+                ],
+                'body' => [
+                    'qualifier' => 'VIDEO'
+                ]
+            ]);
+        } catch (ClientException $e) {
+            throw new CannotGenerateUploadUrlException();
+        }
+
+        $body = json_decode($res->getBody());
+
+        return $body['uploadUrl'];
     }
 }
